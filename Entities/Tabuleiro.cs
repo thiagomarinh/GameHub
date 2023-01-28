af448cce34c3ad162669e8b1732675731665881b
Thiago Marinho <thiagoo_100@hotmail.com>
2023-01-28 14:01:22 -0300

    implementando o metodo FazerJogada

--- Entities/Tabuleiro.cs
+++ Entities/Tabuleiro.cs
@@ -6,6 +6,7 @@ namespace GameHub.Entities
         public int Linhas { get; set; }
         public int Colunas { get; set; }
 
+        public string[,] matriz = { { "7", "8", "9" }, { "4", "5", "6" }, { "1", "2", "3" } };
         public Tabuleiro(int linhas, int colunas)
         {
             Linhas = linhas;
@@ -16,11 +17,36 @@ namespace GameHub.Entities
         {
             for (int i = 0; i < Linhas; i++)
             {
+                Console.Write("|");
                 for (int j = 0; j < Colunas; j++)
                 {
-                    Console.WriteLine("X");
+                    Console.Write($" {matriz[i,j]} |");
+                    
                 }
+                Console.WriteLine("");
+            }
+        }
 
+        // metodo fazer jogada verifica qual 
+        public void FazerJogada(string jogada, string peca)
+        {
+            for (int i = 0; i < Linhas; i++)
+            {
+                for (int j = 0; j < Colunas; j++)
+                {
+                    if (matriz[i,j] == jogada && peca == "X")
+                    {
+                        matriz[i, j] = "X";
+                        MostrarTabuleiro();
+                    }
+                    if (matriz[i,j] == jogada && peca == "O")
+                    {
+                        matriz[i, j] = "O";
+                        MostrarTabuleiro();
+                    }
+
+                }
+                
             }
         }
 
--- Program.cs
+++ Program.cs
@@ -3,15 +3,33 @@ using System;
 
 namespace GameHub
 {
-	class Program
-	{
-		public static void Main(string[] args)
-		{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
 
-			Tabuleiro tabuleiro = new Tabuleiro(3, 3);
+            Tabuleiro tabuleiroVelha = new Tabuleiro(3, 3);
 
-			tabuleiro.MostrarTabuleiro();
+            tabuleiroVelha.MostrarTabuleiro();
+            do
+            {
+                Console.Write("Digite o nome do jogador X: ");
+                string jogador1 = Console.ReadLine();
 
-		}
-	}
+                Console.Write("jogada X:");
+                string jogada1 = Console.ReadLine()!;
+                tabuleiroVelha.FazerJogada(jogada1, "X");
+
+                Console.Write("Digite o nome do jogador X: ");
+                string jogador2 = Console.ReadLine();
+
+                Console.Write("Jogada O:");
+                string jogada2 = Console.ReadLine()!;
+                tabuleiroVelha.FazerJogada(jogada2, "O");
+
+            } while (true);
+
+
+        }
+    }
 }
